27d43774dfbc2a0ba20620be78c25990895a157d
David Sun <15901110328@139.com>
2022-08-22 22:11:59 +0800

    新增玩家控制器
    添加0822的备份

--- level design 01/Assets/Script/mouselook.cs
+++ level design 01/Assets/Script/mouselook.cs
@@ -9,10 +9,12 @@ public class mouselook : MonoBehaviour
 
     public Transform playerBody;
 
+    float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -21,6 +23,9 @@ public class mouselook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
